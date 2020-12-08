c84e3c791858b05722d6f28d6e4f6dfee6325bf7
zack alstadt <zralstadt@comcast.net>
2020-12-08 00:52:40 -0800

    Day3 Part 1 and 2 completed

---
+++ AdventOfCode2020/AdventOfCode2020/Days/Day3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Days
+{
+    class Day3 : Day
+    {
+        private readonly int xMovement = 3;
+        private readonly int yMovement = 1;
+
+        private string[] tobogganMap;
+
+        public override void PrepareData(string[] data)
+        {
+            tobogganMap = data;
+        }
+
+        public override string Run()
+        {
+            int x = 0;
+            int y = 0;
+            int numOfEncounteredTrees = 0;
+
+            while (y < tobogganMap.Length)
+            {
+                if(tobogganMap[y][x] == '#')
+                {
+                    numOfEncounteredTrees++;
+                }
+
+                x = (x + xMovement) % tobogganMap[y].Length;
+                y += yMovement;
+            }
+
+            stringBuilder.Append("Part 1 Answer: ").Append(numOfEncounteredTrees).Append(' ', 5);
+            return stringBuilder.ToString();
+        }
+    }
+}
